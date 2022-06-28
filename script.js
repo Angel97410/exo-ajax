b4070f268e53afaac0478be10ffa8749cdd22338
Angélique Robert <angel97410.ar@gmail.com>
2022-06-28 13:17:32 +0200

    maj affichage img+ ajout css

--- script.js
+++ script.js
@@ -6,7 +6,19 @@ function showUsers() {
   xhr.addEventListener('readystatechange', function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
       if (xhr.status === 200) {
-        document.getElementById('allUtilisateurs').innerText = xhr.response
+        // met les données sous forme d'objet
+        const object = JSON.parse(xhr.response)
+
+        let myHtml = ""
+        let image = ""
+        // pour chaque element avec intitulé first_name contenu dans object
+        object.data.forEach(element => {
+          myHtml += '<div class="card col-6" style="width: 18rem; display: flex;" ><img src= ' + element.avatar + ' class="card-img-top"> <h5>' + element.first_name + ' ' + element.last_name + '</h5><p>'+element.email+'</p></div>'
+          console.log(element.avatar)
+        });
+
+        document.getElementById('allUtilisateurs').innerHTML = myHtml
+        // document.getElementById('imageAllUsers').src = image
         console.log("Response = " + xhr.response);
       } else if (xhr.status === 400) {
         alert("Impossible de trouver l'url de la requête AJAX")

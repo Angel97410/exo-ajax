1d9d8d608ebbf9c0a4400f5ff6f31a794ff11d24
Angélique Robert <angel97410.ar@gmail.com>
2022-06-29 11:57:06 +0200

    ajout fonction create avec ajax et fetch

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function getUsers(nbPage) {
   document.getElementById('pagination').innerHTML = ''
   // initialisation requête
   const xhr = new XMLHttpRequest();
-  const url = "https://reqres.in/api/users?delay=3&page=" + nbPage
+  const url = "https://reqres.in/api/users?delay=1&page=" + nbPage
   xhr.open('GET', url);
 
   xhr.addEventListener('readystatechange', function () {
@@ -41,7 +41,7 @@ function getUsers(nbPage) {
 
 // ---------------------------------------------------FONCTION AFFICHAGE NOM + PRENOM + MAIL-----------------------------------------------
 
-function setUsersInPage(listUsers){
+function setUsersInPage(listUsers) {
   let myHtml = ""
   // pour chaque element avec intitulé first_name contenu dans object
   listUsers.data.forEach(element => {
@@ -49,23 +49,86 @@ function setUsersInPage(listUsers){
   });
 
   document.getElementById('allUtilisateurs').innerHTML = myHtml
-  
+
   // Création de la pagination 
   let nbPage = listUsers.total_pages
   let currentPage = listUsers.page
 
   let htmlPagination = ""
-  for (i=1 ; i <= nbPage; i++){
-    if (i == currentPage){
-      htmlPagination += '<button class="btn active" disabled>'+i+'</button>'
-    }else{
-      htmlPagination += '<button class="btn" onclick= "getUsers('+i+')">'+i+'</button>'
+  for (i = 1; i <= nbPage; i++) {
+    if (i == currentPage) {
+      htmlPagination += '<button class="btn active" disabled>' + i + '</button>'
+    } else {
+      htmlPagination += '<button class="btn" onclick= "getUsers(' + i + ')">' + i + '</button>'
     }
   }
   document.getElementById('pagination').innerHTML = htmlPagination
 }
 
 // PERMET D'OUVRIR LA PAGE DIRECTEMENT SUR PAGE 1
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   getUsers(1)
 });
+
+// --------------------------------------------FONCTION CREATION UTILISATEUR AVEC AJAX-------------------------------------------------
+function createUser() {
+
+  // requête POST 
+  const xhr = new XMLHttpRequest();
+  const url = "https://reqres.in/api/users"
+  xhr.open('POST', url);
+
+  //  A ajouter obligatoirement setRequestHeader pour une méthode POST
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.addEventListener('readystatechange', function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 201) {
+        // met les données récupéré sous forme d'objet
+        const object = JSON.parse(xhr.response)
+        console.log(object)
+      } else if (xhr.status === 400) {
+        alert("Impossible de trouver l'url de la requête AJAX")
+      } else {
+        alert("Une erreur est survenue")
+      }
+    };
+  });
+  // Créer le nouvel utilisateur (toujour reprendre ça)
+  let myForm = new FormData()
+  myForm.append('name', 'Marty');
+  myForm.append('job', 'David');
+
+  var object = {};
+  myForm.forEach((value, key) => object[key] = value);
+  var json = JSON.stringify(object);
+  // Jusqu'ici 
+
+  // envoie de la requête
+  xhr.send(json);
+}
+
+// ----------------------------------------------FONCTION CREATION UTILISATEUR AVEC API FETCH-------------------------------------------------------
+// -----------------------------------------------------PLUS SIMPLE COMME METHODE POST ---------------------------------------------------
+function createUserApiFetch() {
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json")
+  const body = JSON.stringify({
+    name: 'David',
+    job: 'Marty',
+  }); //JSON.stringify pour convertir en objet JSON et éviter le risque des caractères spéciaux.
+
+  const init = {
+    method: 'POST',
+    headers: headers,
+    body: body
+  };
+
+  fetch('https://reqres.in/api/users', init)
+    .then(response => {
+      return response.json();
+    })
+    .then(response => {
+      // console.log(response)
+    })
+    .catch(error => alert("Erreur : " + error));
+}
